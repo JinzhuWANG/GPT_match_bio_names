2ae8dff589613923fb11309d3e9e6c798e07b6a0
Jinzhu WANG <wangjinzhulala@gmail.com>
2024-07-13 12:23:17 +1000

    Use request to get species names

--- Step_1_Clearn_CSV_source.py
+++ Step_1_Clearn_CSV_source.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 import openai
+import requests
 
 from tools import clean_csv
+from tqdm.auto import tqdm
 
 
 
-
-# Read data
+# Read data from 
 df_fauna = clean_csv('data/EPBC Act List of Threatened Fauna.csv')
 df_flora = clean_csv('data/EPBC Act List of Threatened Flora.csv')
 df_flora['Group'] = df_flora['Group'].replace('Flora', 'Plants')        # Rename 'Flora' to 'Plants'
@@ -20,49 +21,19 @@ names_luto = df_luto['SPECIES_LUTO'].tolist()
 
 
 
-'''
-Groups of df    ['Birds', 'Mammals', 'Reptiles', 'Plants', 'Frogs', 'Other Animals', 'Fishes']
-Groups of luto  ['Birds', 'Mammals', 'Reptiles', 'Plants', 'Amphibians']
-'''
-
-group_same = ['Birds', 'Mammals', 'Reptiles', 'Plants']
-group_diff = ['Frogs', 'Other Animals', 'Fishes']
-
-matchs = []
-for group in df['Group'].unique():
-    
-    match_from = df.query('`Group` == @group')['Scientific Name'].tolist()
-    
-    match_to_df = df_luto.query('TAXON == @group') if group in group_same else df_luto.query('TAXON == "Amphibians"')
-    match_to = dict(zip(match_to_df['SPECIES_LUTO'], match_to_df['TAXON']))
 
 
 
+species_names = df_luto['SPECIES_LUTO'].tolist()
+base_url = 'https://api.gbif.org/v1/parser/name'
+
+results = []
+for name in tqdm(species_names, total=len(species_names)):
+    response = requests.get(base_url, params={'name': name})
+    if response.status_code == 200:
+        results += response.json()
 
 
-
-# Initialize the OpenAI API key
-from openai import OpenAI
-client = OpenAI()
-n=3
-match_from_str = ', '.join(match_from)
-match_to_str = ', '.join(match_to.keys())[:15000]
-
-completion = client.chat.completions.create(
-  model="gpt-4o",
-  messages=[
-    {"role": "system", 
-     "content": '''You are a professor in biology, 
-                   skilled in the scientific names and classifications for various species.'''},
-    {"role": "user", 
-     "content": f'''Find the all matches from the list: '{match_from_str}' 
-                    from the following list: {match_to_str}. The match must be from the same group as the original species.
-                    Provide the matches and their confidence levels.
-                    
-                    If you can not find a match, please say 'No match'.'''}
-      ]
-)
-
-print(completion.choices[0].message.content)
-
+out = pd.DataFrame(results)
+out.to_csv('data/species_luto_gbif.csv', index=False)
 
